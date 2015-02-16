412fdb5b2c8147c8943d3475b81f928c037eaf9f
vishwesh <vishwesh@acompli.com>
2015-02-16 11:52:17 +0530

    Week 2 assignments and data

---
+++ week2/pollutantmean.R
@@ -0,0 +1,7 @@
+# Created by VJ in RStudio
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    data = lapply(id, function(i) read.csv(paste(directory, "/", formatC(i, 
+        width = 3, flag = "0"), ".csv", sep = ""))[[pollutant]])
+
+    return(mean(unlist(data), na.rm = TRUE))
+}
